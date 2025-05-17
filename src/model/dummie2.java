d514992ff7d5bfcb377f0518b58deead852a58d0
fankybisciut <igorgarashchenko557@gmail.com>
2025-05-17 11:22:27 +0200

    new funtionality requested

--- src/model/dummie2.java
+++ src/model/dummie2.java
@@ -2,4 +2,8 @@ package model;
 
 public class dummie2 {
     private String nameVariableIntegration ="integration branch ";
+
+    public void CallToDummie1(){
+        System.out.println("Call to dummie1");
+    }
 }
